5be680b858ebeab4a513c6edd621827c17ab8eb9
Josh Stephens <gamecleaver@gmail.com>
2024-03-23 20:43:22 +1000

    update data output

--- Testing/Dagger.py
+++ Testing/Dagger.py
@@ -3,31 +3,33 @@ import pandas as pd
 import tkinter as tk
 import customtkinter as ctk
 from src.fetch import api_call
-
 class Dagger:
     def create_window():
         def submit():
             index = index_entry.get()
             name = name_entry.get()
             result = api_call(index, name)
-            print(result)
+            result_label.configure(text=result)
 
         root = ctk.CTk()
         root.title("Dagger")
-        root.geometry("600x400")
+        root.geometry("800x400")
         
         index_label = ctk.CTkLabel(root, text="Subject:")
-        index_label.pack()
+        index_label.grid(row=0, column=0, padx=5, pady=5)
         index_entry = ctk.CTkEntry(root)
-        index_entry.pack()
+        index_entry.grid(row=0, column=1, padx=5, pady=5)
 
         name_label = ctk.CTkLabel(root, text="Search:")
-        name_label.pack()
+        name_label.grid(row=0, column=2, padx=5, pady=5)
         name_entry = ctk.CTkEntry(root)
-        name_entry.pack()
+        name_entry.grid(row=0, column=3, padx=5, pady=5)
 
         submit_button = ctk.CTkButton(root, text="Submit", command=submit)
-        submit_button.pack()
+        submit_button.grid(row=0, column=4, padx=5, pady=5)
+
+        result_label = ctk.CTkLabel(root, text="")
+        result_label.grid(row=1, column=0, columnspan=5)
 
         root.mainloop()
 
