029ae31c9a396b01850224ccad6821b4cfead550
Josh Stephens <gamecleaver@gmail.com>
2024-03-28 13:44:29 +1000

    change to fetch

--- Testing/src/fetch.py
+++ Testing/src/fetch.py
@@ -9,13 +9,7 @@ def api_call(index, name):
     try:
         response = requests.request("GET", url, headers=headers, data=payload)
         data = json.loads(response.text)
-        if index == "classes":
-            result = {key: data[key] for key in ['name', 'hit_die'] if key in data}
-            if 'proficiency_choices' in data:
-                result['proficiency_choices'] = [item['desc'] for item in data['proficiency_choices'] if 'desc' in item]
-            return result
-        else:
-            return data
+        return data
     except Exception as e:
         print(f"An error occurred: {e}")
         return None
